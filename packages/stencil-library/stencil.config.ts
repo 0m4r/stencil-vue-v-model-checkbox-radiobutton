3288055d9b56047983016280c69c5a0a62d4f18c
Omar Adobati <omar.adobati@publicissapient.com>
2023-06-09 10:25:48 +0200

    Initial commit

---
+++ packages/stencil-library/stencil.config.ts
@@ -0,0 +1,28 @@
+import { angularOutputTarget } from '@stencil/angular-output-target';
+
+import { Config } from '@stencil/core';
+
+export const config: Config = {
+  namespace: 'stencil-library',
+  outputTargets: [
+    {
+      type: 'dist',
+      esmLoaderPath: '../loader',
+    },
+    {
+      type: 'dist-custom-elements',
+    },
+    {
+      type: 'docs-readme',
+    },
+    {
+      type: 'www',
+      serviceWorker: null, // disable service workers
+    },
+    angularOutputTarget({
+      componentCorePackage: 'stencil-library',
+      directivesProxyFile: '../angular-workspace/projects/component-library/src/lib/stencil-generated/components.ts',
+      directivesArrayFile: '../angular-workspace/projects/component-library/src/lib/stencil-generated/index.ts',
+    }),
+  ],
+};
